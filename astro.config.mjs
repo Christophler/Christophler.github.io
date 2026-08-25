4fc3bda136ffeca576db7dad233cdf8a80a351b0
Christophler <chris.lpher15@gmail.com>
2026-08-24 20:10:38 -0400

    Add writeup detail layout with syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://christophler.github.io',
   base: '/',
   output: 'static',
+  integrations: [mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark-default',
+    },
+  },
 });
